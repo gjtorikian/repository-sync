3f30ba4b233f0688c0c6ee7dec60aefd9db3d228
Garen Torikian <gjtorikian@gmail.com>
2014-03-18 17:44:19 -0700

    Check (and process) against the master branch only

--- app.rb
+++ app.rb
@@ -17,6 +17,13 @@ class RepositorySync < Sinatra::Base
 
   post "/update_public" do
     if params[:token] == ENV["token"]
+      payload = params[:payload]
+
+      unless master_branch?(payload)
+        Rails.logger.info "WEBHOOK: Payload was not for master, aborting."
+        return
+      end
+
       "Hey, you did it!"
     else
       "Tokens didn't match!"
@@ -25,9 +32,24 @@ class RepositorySync < Sinatra::Base
 
   post "/update_private" do
     if params[:token] == ENV["token"]
+      payload = params[:payload]
+
+      unless master_branch?(payload)
+        Rails.logger.info "WEBHOOK: Payload was not for master, aborting."
+        return
+      end
+
       "Hey, you did it, privately!"
     else
       "Tokens didn't match!"
     end
+  end
+
+  helpers do
+
+    master_branch?(payload)
+      payload["ref"] == "refs/heads/master"
+    end
+
   end
 end
