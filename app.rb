9872f0230468eba1cbe42a7dd6f72382f830fea4
Garen Torikian <gjtorikian@gmail.com>
2014-06-09 20:44:49 -0700

    Digest::Digest is deprecated

--- app.rb
+++ app.rb
@@ -52,7 +52,7 @@ class RepositorySync < Sinatra::Base
   helpers do
 
     def verify_signature(payload_body)
-      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV['SECRET_TOKEN'], payload_body)
       return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
     end
 
